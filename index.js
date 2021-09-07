195b3330c0b849fc5635a488ac609b246fb86a29
Sven Helmberger <fforw@gmx.de>
2021-09-07 19:44:59 +0200

    Upgrade to Webpack 5
    Remove cleaning, it's done by wepack now.
    
    Switches to the new hook tapping

--- index.js
+++ index.js
@@ -28,55 +28,21 @@ function onBuildDone(stats)
 {
     try
     {
-
         const buildDir = this.buildDir;
         const opts = this.opts;
-        const suffixes = opts.clean;
 
-        if (suffixes && suffixes.length)
+        const data = stats.toJson();
+
+        const assetsPath = path.resolve(buildDir, "webpack-assets.json");
+        if (opts.debug)
         {
-            const newlyCreatedAssets = stats.compilation.assets;
-
-            const unlinked = [];
-            fs.readdir(path.resolve(buildDir), (err, files) => {
-
-                if (err)
-                {
-                    throw err;
-                }
-
-                files.forEach(file => {
-
-                    if (endsWithOneOf(file, suffixes) && !newlyCreatedAssets[file])
-                    {
-                        fs.unlinkSync(path.resolve(buildDir, file));
-                        unlinked.push(file);
-                    }
-                });
-
-                if (opts.debug && unlinked.length > 0)
-                {
-                    console.log("Removed old assets: ", unlinked);
-                }
-            });
+            console.log("Writing webpack assets to", assetsPath);
         }
 
-        if (opts.generateAssetsJson)
-        {
-            const data = stats.toJson();
-
-            const assetsPath = path.resolve(buildDir, "webpack-assets.json");
-            if (opts.debug)
-            {
-                console.log("Writing webpack assets to", assetsPath);
-            }
-
-            fs.writeFileSync(
-                assetsPath,
-                JSON.stringify(data.entrypoints, null, 4)
-            );
-
-        }
+        fs.writeFileSync(
+            assetsPath,
+            JSON.stringify(data.entrypoints, null, 4)
+        );
     }
     catch (e)
     {
@@ -85,11 +51,6 @@ function onBuildDone(stats)
 }
 
 const DEFAULT_OPTS = {
-
-    generateAssetsJson: true,
-
-    /** file name endings to clean.*/
-    clean: [".js", ".js.map", ".css", ".css.map"],
 
     /** if true, print report on deletion */
     debug: false
@@ -104,7 +65,7 @@ function WebpackAssetsPlugin(opts)
 WebpackAssetsPlugin.prototype.apply = function (compiler) {
     this.buildDir = compiler.options.output.path;
 
-    compiler.plugin("done", this.onBuildDone);
+    compiler.hooks.done.tap("jsview-plugin", this.onBuildDone);
 };
 
 module.exports = WebpackAssetsPlugin;
